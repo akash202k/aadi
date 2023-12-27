63ea04309cf52d6a8e0d715e361ccb0dee482fd5
Akash Pawar <akashpawar202k@gmail.com>
2023-12-27 16:12:07 +0530

    q1

--- assignments/q1.js
+++ assignments/q1.js
@@ -1,1 +1,4 @@
-// 
+// write a function which has name findGreaterNo 
+// the function will accept two parameter value1 and value2
+// and return greater No 
+// finally call that function and print greater No
